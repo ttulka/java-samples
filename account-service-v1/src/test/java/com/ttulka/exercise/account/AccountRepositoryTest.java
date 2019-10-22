f2b3cbddba9cf5d71c3aaa74a409cb8b28c42659
Tomas Tulka <tomas.tulka@gmail.com>
2019-10-22 07:30:25 +0200

    test classes renamed

--- account-service-v1/src/test/java/com/ttulka/exercise/account/AccountRepositoryTest.java
+++ account-service-v1/src/test/java/com/ttulka/exercise/account/AccountRepositoryTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class PersistenceTest {
+class AccountRepositoryTest {
 
     @Test
     void valid_entity_is_saved() {
--- account-service-v1/src/test/java/com/ttulka/exercise/account/AccountTest.java
+++ account-service-v1/src/test/java/com/ttulka/exercise/account/AccountTest.java
@@ -1,11 +1,6 @@
 package com.ttulka.exercise.account;
 
 import java.time.ZonedDateTime;
-import java.util.UUID;
-
-import com.ttulka.exercise.account.exception.AccountNotFoundException;
-import com.ttulka.exercise.account.exception.InvalidLoginException;
-import com.ttulka.exercise.account.exception.UsernameAlreadyExistsException;
 
 import org.junit.jupiter.api.Test;
 
@@ -91,97 +86,5 @@ class AccountTest {
         account.login();
 
         assertThat(account.hasLoggedInSince(ZonedDateTime.now().plusSeconds(1))).isFalse();
-    }
-
-    @Test
-    void registered_account_with_correct_password_can_log_in() {
-        AccountService accountService = new RegisteredAccountService(new InMemoryAccountRepository());
-
-        String username = UUID.randomUUID().toString();
-        accountService.register(username, "test@example.com", "pwd1");
-
-        Account account = accountService.login(username, "pwd1");
-        assertThat(account).isNotNull();
-    }
-
-    @Test
-    void once_registered_a_username_cannot_be_registered_again() {
-        AccountService accountService = new RegisteredAccountService(new InMemoryAccountRepository());
-
-        String username = UUID.randomUUID().toString();
-        accountService.register(username, "test@example.com", "pwd1");
-
-        assertThrows(UsernameAlreadyExistsException.class, () ->
-                accountService.register(username, "test@example.com", "pwd1"));
-    }
-
-    @Test
-    void non_existing_account_cannot_log_in() {
-        AccountService accountService = new RegisteredAccountService(new InMemoryAccountRepository());
-
-        assertThrows(AccountNotFoundException.class, () ->
-                accountService.login("this user does not exist", "password"));
-    }
-
-    @Test
-    void existing_account_with_wrong_password_cannot_log_in() {
-        AccountService accountService = new RegisteredAccountService(new InMemoryAccountRepository());
-
-        String username = UUID.randomUUID().toString();
-        accountService.register(username, "test@example.com", "pwd1");
-
-        assertThrows(InvalidLoginException.class, () ->
-                accountService.login(username, "wrong password"));
-    }
-
-    @Test
-    void deleted_account_cannot_log_in() {
-        AccountService accountService = new RegisteredAccountService(new InMemoryAccountRepository());
-
-        String username = UUID.randomUUID().toString();
-        accountService.register(username, "test@example.com", "pwd1");
-
-        accountService.unregister(username);
-
-        assertThrows(AccountNotFoundException.class, () ->
-                accountService.login(username, "pwd1"));
-    }
-
-    @Test
-    void account_is_logged_in_since_before_logged_in_for_the_last_time() {
-        AccountService accountService = new RegisteredAccountService(new InMemoryAccountRepository());
-
-        String username = UUID.randomUUID().toString();
-        accountService.register(username, "test@example.com", "pwd1");
-        ZonedDateTime beforeLogin = ZonedDateTime.now().minusSeconds(1);
-        accountService.login(username, "pwd1");
-
-        boolean loggedIn = accountService.hasLoggedInSince(username, beforeLogin);
-        assertThat(loggedIn).isTrue();
-    }
-
-    @Test
-    void account_not_logged_in_since_after_logged_in_for_the_last_time() {
-        AccountService accountService = new RegisteredAccountService(new InMemoryAccountRepository());
-
-        String username = UUID.randomUUID().toString();
-        accountService.register(username, "test@example.com", "pwd1");
-        accountService.login(username, "pwd1");
-        ZonedDateTime afterLogin = ZonedDateTime.now().plusSeconds(1);
-
-        boolean loggedIn = accountService.hasLoggedInSince(username, afterLogin);
-        assertThat(loggedIn).isFalse();
-    }
-
-    @Test
-    void account_not_logged_in_since_now_after_been_registered() {
-        AccountService accountService = new RegisteredAccountService(new InMemoryAccountRepository());
-
-        String username = UUID.randomUUID().toString();
-        accountService.register(username, "test@example.com", "pwd1");
-        ZonedDateTime afterRegistration = ZonedDateTime.now().plusSeconds(1);
-
-        boolean loggedInAfter = accountService.hasLoggedInSince(username, afterRegistration);
-        assertThat(loggedInAfter).isFalse();
     }
 }

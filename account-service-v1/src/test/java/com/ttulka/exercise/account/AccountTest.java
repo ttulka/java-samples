eb46a8995eab76a0ef8d26a84c2ec3e5b527a817
Tomas Tulka <tomas.tulka@gmail.com>
2019-10-22 07:28:24 +0200

    Account tests added

--- account-service-v1/src/test/java/com/ttulka/exercise/account/AccountTest.java
+++ account-service-v1/src/test/java/com/ttulka/exercise/account/AccountTest.java
@@ -33,6 +33,67 @@ class AccountTest {
     }
 
     @Test
+    void encrypted_password_is_created() {
+        Account account = new Account("test", "test@example.com", "pwd1");
+
+        assertThat(account.getEncryptedPassword()).isNotNull();
+    }
+
+    @Test
+    void salt_is_generated() {
+        Account account = new Account("test", "test@example.com", "pwd1");
+
+        assertThat(account.getSalt()).isNotNull();
+    }
+
+    @Test
+    void can_login_with_a_valid_password() {
+        Account account = new Account("test", "test@example.com", "pwd1");
+
+        assertThat(account.canLogin("pwd1")).isTrue();
+    }
+
+    @Test
+    void cannot_login_with_an_invalid_password() {
+        Account account = new Account("test", "test@example.com", "pwd1");
+
+        assertThat(account.canLogin("xxx")).isFalse();
+    }
+
+    @Test
+    void valid_password_is_changed() {
+        Account account = new Account("test", "test@example.com", "pwd1");
+        account.changePassword("updated");
+
+        assertThat(account.canLogin("pwd1")).isFalse();
+        assertThat(account.canLogin("updated")).isTrue();
+    }
+
+    @Test
+    void login_sets_last_logged_in() {
+        Account account = new Account("test", "test@example.com", "pwd1");
+        account.login();
+
+        assertThat(account.getLastLoggedIn()).isNotNull();
+    }
+
+    @Test
+    void has_logged_in_since_before_login() {
+        Account account = new Account("test", "test@example.com", "pwd1");
+        account.login();
+
+        assertThat(account.hasLoggedInSince(ZonedDateTime.now().minusSeconds(1))).isTrue();
+    }
+
+    @Test
+    void has_not_logged_in_since_after_login() {
+        Account account = new Account("test", "test@example.com", "pwd1");
+        account.login();
+
+        assertThat(account.hasLoggedInSince(ZonedDateTime.now().plusSeconds(1))).isFalse();
+    }
+
+    @Test
     void registered_account_with_correct_password_can_log_in() {
         AccountService accountService = new RegisteredAccountService(new InMemoryAccountRepository());
 

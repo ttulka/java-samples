8a00d6fb01ce0d372904d2ec0566abcce47f2366
Tomas Tulka <tomas.tulka@gmail.com>
2020-07-24 10:03:11 +0200

    mqtt java client

--- mqtt/client/src/main/java/com/ttulka/samples/mqtt/MqttApplication.java
+++ mqtt/client/src/main/java/com/ttulka/samples/mqtt/MqttApplication.java
@@ -2,16 +2,19 @@ package com.ttulka.samples.mqtt;
 
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-public class MqttPublisherApp {
+public class MqttApplication {
 
     public static void main(String[] args) throws MqttException {
         MqttClient client = new MqttClient("tcp://localhost:1883", MqttClient.generateClientId());
-        client.connect();
-        MqttMessage message = new MqttMessage();
-        message.setPayload("Hello world from Java".getBytes());
-        client.publish("test1", message);
-        client.disconnect();
+        try {
+            client.connect();
+
+            MqttPublisher publisher = new MqttPublisher(client, "test1");
+            publisher.publish("Hello world from Java".getBytes());
+
+        } finally {
+            client.disconnect();
+        }
     }
 }
--- mqtt/client/src/main/java/com/ttulka/samples/mqtt/MqttPublisher.java
+++ mqtt/client/src/main/java/com/ttulka/samples/mqtt/MqttPublisher.java
@@ -1,4 +1,23 @@
 package com.ttulka.samples.mqtt;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+@RequiredArgsConstructor
 public class MqttPublisher {
+
+    private final @NonNull MqttClient client;
+    private final @NonNull String topic;
+
+    public void publish(byte[] message) {
+        try {
+            client.publish(topic, new MqttMessage(message));
+
+        } catch (MqttException e) {
+            throw new RuntimeException("Cannot publish a message", e);
+        }
+    }
 }
